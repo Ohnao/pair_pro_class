b0d132efc8a0efc0ab4b74f7246611836fd07ef6
Ohnao <naoya.ohsaki@gaiax.com>
2020-07-14 16:11:12 +0900

    条件式をリファクタ

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,12 @@
 (1..100).each do |num|
-    if(num % 3 == 0 && num % 5 == 0)
-        puts "FizaaBuzz"
-    elsif(num % 5 == 0)
-        puts "Buzz"
-    elsif(num % 3 == 0)
-        puts "Fizz"
-    else
-        puts num
-    end
+	if num % 3 == 0 \
+		&& num % 5 == 0
+		puts "FizaaBuzz"
+	elsif num % 5 == 0
+		puts "Buzz"
+	elsif num % 3 == 0
+		puts "Fizz"
+	else
+		puts num
+	end
 end
