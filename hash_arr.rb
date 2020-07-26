9ee1730912b4b362e72510c5e37ea71d56549dda
Ohnao <naoya.ohsaki@gaiax.com>
2020-07-26 19:19:18 +0900

    ハッシュの中にハッシュを入れたデータ管理コードのサンプル

---
+++ hash_arr.rb
@@ -0,0 +1,26 @@
+access_hash = {
+    "Joutarou"  => 1,
+    "Avdol" => 3,
+    "Kakyouin" => 2,
+    "Joseph"  => 8
+}
+  
+  ruikei_hash = {
+    "Joutarou" => 12,
+    "Avdol" => 12,
+    "Kakyouin" => 100,
+    "Joseph" => 22
+  }
+  
+  matome_hash = {}
+  
+  access_hash.keys.each do |name|
+    matome_hash[name] = {
+      "access" => access_hash[name],
+      "ruikei" => ruikei_hash[name]
+    }
+  end
+  
+  p matome_hash
+  
+  p matome_hash["Joutarou"]["ruikei"]
