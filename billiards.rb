22cc305a183ceb6ef862c32176a76b37a0228f2e
Ohnao <naoya.ohsaki@gaiax.com>
2020-07-26 19:17:22 +0900

    ハッシュを使ったコードに修正

--- billiards.rb
+++ billiards.rb
@@ -1,3 +1,40 @@
+def move_ball(hash)
+	hash["x"] += hash["x_way"]
+	hash["y"] += hash["y_way"]
+	hash["step"] += 1
+end
+
+def reflect_x(hash)
+	if hash["x_way"] == 1
+		hash["x_way"] = -1
+  elsif hash["x_way"] == -1
+    hash["x_way"] = 1
+  end
+end
+
+def reflect_y(hash)
+	if hash["y_way"] == 1
+		hash["y_way"] = -1
+  elsif hash["y_way"] == -1
+    hash["y_way"] = 1
+  end
+end
+
+def goal?(hash, x_max, y_max)
+  hash["y"] == 1 && hash["x"] == x_max \
+  || hash["x"] == 1 && hash["y"] == y_max \
+  || hash["x"] == 1 && hash["y"] == 1 \
+  || hash["x"] == x_max && hash["y"] == y_max
+end
+
+def boundary_x?(hash, x_max)
+  hash["x"] == x_max || hash["x"] == 1
+end
+
+def boundary_y?(hash, y_max)
+  hash["y"] == y_max || hash["y"] == 1
+end
+
 x_max = ARGV[0]
 y_max = ARGV[1]
 
@@ -10,32 +47,27 @@ x_max =  x_max.to_i
 y_max =  y_max.to_i
 
 #状態
-x = 1
-y = 1
-step = 1
-x_way = 1
-y_way = 1
+state = {
+  "x" => 1,
+  "y" => 1,
+  "step" => 1,
+  "x_way" => 1,
+  "y_way" => 1
+}
 
-puts " #{step} (#{x},#{y})"
-x += 1
-y += 1
+puts " #{state["step"]} (#{state["x"]},#{state["y"]})"
+
 loop do
-  step += 1
-  puts " #{step} (#{x},#{y})"
+  move_ball(state)
 
-  if y == 1 && x == x_max || x == 1 && y == y_max || x == 1 && y == 1 || x == x_max && y == y_max
+  puts " #{state["step"]} (#{state["x"]},#{state["y"]})"
+
+  if goal?(state, x_max, y_max)
     puts "GOAL!!"
     break
-  elsif x == x_max
-    x_way = -1
-  elsif y == y_max
-    y_way = -1
-  elsif x == 1
-    x_way = 1
-  elsif y == 1
-    y_way = 1
+  elsif boundary_x?(state, x_max)
+    reflect_x(state)
+  elsif boundary_y?(state, y_max)
+    reflect_y(state)
   end
-
-  x += x_way
-  y += y_way
 end
