423abe5672a9dbb90c07b2a27b7fd759a2e03dab
Ohnao <naoya.ohsaki@gaiax.com>
2020-08-10 02:12:36 +0900

    配列のサンプルコードを更新しました

--- sample.rb
+++ sample.rb
@@ -7,7 +7,7 @@
 #end
 #p num
 
-string_numbers = ["123", "234", "345", "456"]
+#string_numbers = ["123", "234", "345", "456"]
 # ↓
 #integer_numbers = [123, 234, 345, 456] # これを作って欲しい
 
@@ -17,17 +17,31 @@ string_numbers = ["123", "234", "345", "456"]
 # end
 # each ではなく map で
 
-integer_numbers = string_numbers.map {|num| num.to_i}
-sum = integer_numbers.sum
-
-
-numbers1 =  [125, 34, 45, 50, 245, 4, 654, 10, 123, 1000]
+#integer_numbers = string_numbers.map {|num| num.to_i}
+#sum = integer_numbers.sum
+#
+#
+#numbers1 =  [125, 34, 45, 50, 245, 4, 654, 10, 123, 1000]
 
 #hundred_num =  numbers1.filter {|num| num / 100 < 10 && num / 100 != 0}
 #p hundred_num
 
-hundred_num2 =  numbers1.filter {|num| num.to_s.size == 3 }
-p hundred_num2
+#hundred_num2 =  numbers1.filter {|num| num.to_s.size == 3 }
+#p hundred_num2
+#
+#hundred_num3 =  numbers1.filter {|num| 100 <= num && num < 1000 }
+#p hundred_num2
 
-hundred_num3 =  numbers1.filter {|num| 100 <= num && num < 1000 }
-p hundred_num2
+numbers = [111, 222, 333]
+
+mapping_number = numbers.map {|num| num ** 2}
+p mapping_number
+
+#numbers =  [111, 222, 333] 
+#count = 0
+#numbers.each do |num|
+#    numbers[count] = numbers[count] ** 2
+#    count += 1
+#end
+#
+#p numbers
