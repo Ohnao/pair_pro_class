45f6ce5b2d2b422cd77ba3d5cbc47f9795e96173
Ohnao <naoya.ohsaki@gaiax.com>
2020-08-11 22:58:24 +0900

    ダブりはあるが、ランキング同列を表示できるようになった

--- tpk_assignment/menu_rank.rb
+++ tpk_assignment/menu_rank.rb
@@ -8,12 +8,38 @@ STDIN.each do |line|
 	menu[line] += 1
 end
 
-count = 1
+#カウントのさせ方別解
+#STDIN.each do |line|
+#	line.chomp!
+#  line.gsub!(/\d+\/\d+\/\d+\t/, '')
+#	line.gsub!(/〜.*/, '')
+#
+#	menu[line] ||= 0
+#	menu[line] += 1
+#end
+
+ranking_count = 1
+stopping_count = 0
+pre_value = []
+pre_count = 0
 ranking_menu = menu.sort_by { |k, v| -v }.to_h
-p ranking_menu
+
 ranking_menu.keys.each do |menu_name|
-	puts "#{count}位 #{menu_name}"
-	count += 1
+	value = ranking_menu[menu_name]
+	pre_value.push value
+
+	if value != pre_value[pre_count]
+		ranking_count += stopping_count
+		puts "#{ranking_count}位 #{menu_name} #{value}件"
+		ranking_count += 1
+		stopping_count = 0
+		pre_count += 1
+	end
+
+	if value == pre_value[pre_count]
+		puts "#{ranking_count}位 #{menu_name} #{value}件"
+		stopping_count += 1
+	end
 end
 
 # 別解の文字列処理
