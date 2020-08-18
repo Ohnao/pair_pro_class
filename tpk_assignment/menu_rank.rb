8c43517e6a317ac8f5ff74a3ada3ec66d223bc4f
Ohnao <naoya.ohsaki@gaiax.com>
2020-08-18 15:21:40 +0900

    メニュー名とメニューの出た数を入れたハッシュの名前と、メニュー名の変数名を変更

--- tpk_assignment/menu_rank.rb
+++ tpk_assignment/menu_rank.rb
@@ -1,11 +1,11 @@
-menu = {}
-menu.default = 0
+menu_count = {}
+menu_count.default = 0
 STDIN.each do |line|
 	line.chomp!
-	line.gsub!(/\d+\/\d+\/\d+\t/, '')
-	line.gsub!(/〜.*/, '')
+	menu_name = line.gsub(/\d+\/\d+\/\d+\t/, '')
+	menu_name.gsub!(/〜.*/, '')
 
-	menu[line] += 1
+	menu_count[menu_name] += 1
 end
 
 #カウントのさせ方別解
@@ -23,10 +23,10 @@ rank = 2
 stopping_count = 0
 history_times = []
 history_menu = []
-pre_count = 0
-pre_menu_count = 0
-rankings = menu.sort_by { |k, v| -v }
-ranking_menu = menu.sort_by { |k, v| -v }.to_h
+pre_index = 0
+pre_menu_index = 0
+rankings = menu_count.sort_by { |k, v| -v }
+ranking_menu = menu_count.sort_by { |k, v| -v }.to_h
 
 puts "1位 #{rankings[0][0]} #{rankings[0][1]}件"
 
@@ -35,16 +35,16 @@ ranking_menu.keys.each do |menu_name|
 	history_times.push times
 	history_menu.push menu_name
 
-	if times != history_times[pre_count]
+	if times != history_times[pre_index]
 		rank += stopping_count
 		stopping_count = 0
-		pre_count += 1
+		pre_index += 1
 	end
 
-	if times == history_times[pre_count] && menu_name != history_menu[pre_menu_count]
+	if times == history_times[pre_index] && menu_name != history_menu[pre_menu_index]
 		puts "#{rank}位 #{menu_name} #{times}件"
 		stopping_count += 1
-		pre_menu_count += 1
+		pre_menu_index += 1
 	end
 end
 
