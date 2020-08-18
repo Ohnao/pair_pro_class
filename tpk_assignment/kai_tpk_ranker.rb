a5c50f1768889dd80c1af178d4c9cb9ecac40596
Ohnao <naoya.ohsaki@gaiax.com>
2020-08-18 18:32:38 +0900

    menu_nameをキーとするメニューのハッシュを作成

--- tpk_assignment/kai_tpk_ranker.rb
+++ tpk_assignment/kai_tpk_ranker.rb
@@ -1,7 +1,11 @@
-
+menu = {}
 STDIN.each do |line|
 	line.chomp!
 	(date, menu_name) = *line.split("\t")
 	menu_name.gsub!(/〜.*/, '')
-	menu_name
+	menu[date] = {"menu_name" => menu_name}
+end
+
+menu.keys.each do |key|
+	p menu[key]
 end
