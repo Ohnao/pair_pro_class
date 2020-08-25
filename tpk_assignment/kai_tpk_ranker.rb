f7c8ba126e2579fd84a39ec6d69fcbe1c86decc0
Ohnao <naoya.ohsaki@gaiax.com>
2020-08-25 15:15:45 +0900

    menu_rankingという一時変数を削除、後置ifによってbreak分を設置

--- tpk_assignment/kai_tpk_ranker.rb
+++ tpk_assignment/kai_tpk_ranker.rb
@@ -7,15 +7,11 @@ STDIN.each do |line|
 	menu[menu_name] += 1
 end
 
-menu_ranking = menu.sort_by { |k, v| -v }.to_h
-
 rank = 1
 previous_time, previous_rank = 0, 0
-menu_ranking.keys.each do |key|
-	if menu_ranking[key] <= 2
-		break
-	end
-	current_time = menu_ranking[key]
+menu.sort_by { |k, v| -v }.to_h.keys.each do |key|
+	break if menu[key] <= 2
+	current_time = menu[key]
 	current_rank = 
 		current_time == previous_time ? previous_rank : rank
 	puts "#{current_rank}位 #{key} #{current_time}件"
--- tpk_assignment/sample.perl
+++ tpk_assignment/sample.perl
@@ -5,7 +5,7 @@ for my $title (sort { $seen{$b} <=> $seen{$a} } keys %seen ) {
    #last if $rank > 30;
    my $current_times = $seen{$title};
    my $current_rank  =
-   $current_times == $previous_times ? $previous_rank : $rank;
+      $current_times == $previous_times ? $previous_rank : $rank;
    printf "%d位: (%d回) %s\n", $current_rank, $seen{$title}, $title;
    ($previous_times, $previous_rank) = ($current_times, $current_rank);
    $rank++;
