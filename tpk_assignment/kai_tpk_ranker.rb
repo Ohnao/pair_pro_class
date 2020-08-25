435ec7b51d2ed9cb088f3617b5b680ebaeb72020
Ohnao <naoya.ohsaki@gaiax.com>
2020-08-25 14:17:09 +0900

    尾形さんの課題、PerlのコードをRubyで書き換える 分割代入

--- tpk_assignment/kai_tpk_ranker.rb
+++ tpk_assignment/kai_tpk_ranker.rb
@@ -1,11 +1,24 @@
 menu = {}
+menu.default = 0
 STDIN.each do |line|
 	line.chomp!
 	(date, menu_name) = *line.split("\t")
 	menu_name.gsub!(/〜.*/, '')
-	menu[date] = {"menu_name" => menu_name}
+	menu[menu_name] += 1
 end
 
-menu.keys.each do |key|
-	p menu[key]
+menu_ranking = menu.sort_by { |k, v| -v }.to_h
+
+rank = 1
+previous_time, previous_rank = 0, 0
+menu_ranking.keys.each do |key|
+	if menu_ranking[key] <= 2
+		break
+	end
+	current_time = menu_ranking[key]
+	current_rank = 
+		current_time == previous_time ? previous_rank : rank
+	puts "#{current_rank}位 #{key} #{current_time}件"
+	previous_time, previous_rank = current_time, current_rank
+	rank += 1
 end
---
+++ tpk_assignment/sample.perl
@@ -0,0 +1,12 @@
+my $rank = 1;
+my ($previous_times, $previous_rank) = (0, 0);
+for my $title (sort { $seen{$b} <=> $seen{$a} } keys %seen ) {
+   last if $seen{$title} <= 2;
+   #last if $rank > 30;
+   my $current_times = $seen{$title};
+   my $current_rank  =
+   $current_times == $previous_times ? $previous_rank : $rank;
+   printf "%d位: (%d回) %s\n", $current_rank, $seen{$title}, $title;
+   ($previous_times, $previous_rank) = ($current_times, $current_rank);
+   $rank++;
+}
