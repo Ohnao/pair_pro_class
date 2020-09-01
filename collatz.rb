2a50dbe8a3a338a91ec5502c2b80dec7c7e9bb2f
Ohnao <naoya.ohsaki@gaiax.com>
2020-09-01 14:59:01 +0900

    pをputsにし、重複したputsの処理を1つに修正

--- collatz.rb
+++ collatz.rb
@@ -3,11 +3,10 @@ number = ARGV[0].to_i
 while number != 1
   if number % 2 == 0
     number = number / 2
-    p number
   else
     number = (number * 3) + 1
-    p number
   end
+  puts number
 end
 
-p "Congraturation!"
+puts "Congraturation!"
