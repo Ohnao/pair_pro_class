f9cd13c862e5dc311a4834de20aea75b12443dd3
Ohnao <noblesse.oblige7@suou.waseda.jp>
2020-10-06 17:32:22 +0900

    年号を西暦に変換するコード

---
+++ guess-ad-year.rb
@@ -0,0 +1,22 @@
+FIRST_YEAR = {
+  "平成" => 1989,
+  "令和" => 2019
+}
+
+input = ARGV[0]
+
+#正規表現の書き換え
+#input =~ /^(..)(\d+|元)年$/
+#jp_era = $1
+#jp_year = $2
+
+#matchメソッドは、キャプチャした文字列をmatchオブジェクトとして値を返し、値へのアクセス方法は[]。
+match = input.match(/^(..)(\d+|元)年$/)
+jp_era = match[1]
+jp_year = match[2]
+
+jp_year = (jp_year == "元") ? 1 : jp_year.to_i
+
+year = FIRST_YEAR[jp_era]
+
+p "西暦#{year + (jp_year - 1)}年"
