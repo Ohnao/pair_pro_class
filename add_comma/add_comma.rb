83ef17e3b60f8138619a72720b7d957e2d863c41
Ohnao <naoya.ohsaki@gaiax.com>
2020-11-10 15:53:25 +0900

    文章中から数字を正規表現で見つけ出し、カンマを三桁区切りにした状態にした文章として返す

--- add_comma/add_comma.rb
+++ add_comma/add_comma.rb
@@ -5,5 +5,8 @@ if input.nil?
   exit 1
 end
 
-#後ろから桁数を指定するのにリバースさせ、正規表現で最小桁数と最大桁数を指定し、scanでマッチした部分へjoinメソッドを用いてカンマを突っ込み、最後に元に戻す
-puts input.reverse.scan(/.{1,3}/).join(",").reverse
+result = input.gsub(/\d*/)do |s|
+  s.reverse.scan(/\d{1,3}/).join(",").reverse
+end
+
+puts result
